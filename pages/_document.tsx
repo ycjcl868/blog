554d873f2f3e9430150772054f1c4f00e2c2fadb
ycjcl868 <45808948@qq.com>
2021-03-10 22:19:47 +0800

    chore: ga id

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -61,11 +61,23 @@ export default class MyDocument extends Document {
               />
               <script
                 dangerouslySetInnerHTML={{
-                  __html: `window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-
-              gtag('config', '${ga.id}');`
+                  __html: `(function(){
+                    if (!location.port) {
+                      (function (i, s, o, g, r, a, m) {
+                        i['GoogleAnalyticsObject'] = r;
+                        i[r] = i[r] || function () {
+                            (i[r].q = i[r].q || []).push(arguments)
+                          }, i[r].l = 1 * new Date();
+                        a = s.createElement(o),
+                          m = s.getElementsByTagName(o)[0];
+                        a.async = 1;
+                        a.src = g;
+                        m.parentNode.insertBefore(a, m)
+                      })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+                      ga('create', '${ga.id}', 'auto');
+                      ga('send', 'pageview');
+                    }
+                  })();`
                 }}
               />
             </>
