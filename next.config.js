af5ac856815ea0fa98697770315d4ab6dd5551df
ycjcl868 <chaolinjin@gmail.com>
2021-11-03 14:11:43 +0800

    chore: upgrade nextjs 12 enable swc, using pnpm

--- next.config.js
+++ next.config.js
@@ -1,12 +1,5 @@
 // const isDev = process.env.NODE_ENV === 'development' || !process.env.NODE_ENV
 
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true'
-})
-
-module.exports = withBundleAnalyzer({
-  images: {
-    loader: 'imgix',
-    path: ''
-  }
-})
+module.exports = {
+  swcMinify: true
+}
