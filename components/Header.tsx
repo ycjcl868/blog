e8ea91d0713b1aac3e1fd50c08a6e154ebfa6da8
ycjcl868 <chaolinjin@gmail.com>
2021-12-23 17:23:56 +0800

    chore: upgrade next

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,11 +9,12 @@ const NavBar = () => {
   const locale = useLocale()
 
   const links = [
-    { id: 0, name: locale.NAV.INDEX, to: BLOG.path || '/', show: true },
-    { id: 1, name: locale.NAV.ABOUT, to: '/about', show: BLOG.showAbout },
-    { id: 2, name: locale.NAV.SEARCH, to: '/search', show: true },
-    { id: 3, name: locale.NAV.RSS, to: '/feed', show: true }
-  ]
+    { name: locale.NAV.INDEX, to: BLOG.path || '/', show: true },
+    { name: locale.NAV.ABOUT, to: '/about', show: BLOG.showAbout },
+    { name: locale.NAV.SEARCH, to: '/search', show: true },
+    { name: locale.NAV.RSS, to: '/feed', show: true }
+  ].map((link, id) => ({ ...link, id }))
+
   return (
     <div className='shrink-0'>
       <ul className='flex flex-row items-center'>
