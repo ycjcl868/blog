7cc8d9b919e871552498542f33eb9fbba23b9ffd
ycjcl868 <chaolinjin@gmail.com>
2022-12-18 09:37:17 +0800

    chore: max width

--- components/Container.tsx
+++ components/Container.tsx
@@ -73,7 +73,7 @@ const Container = ({ children, layout, fullWidth, ...customMeta }) => {
         />
         <main
           className={`m-auto flex-grow w-full transition-all ${
-            !fullWidth ? 'max-w-2xl px-4 md:px-0' : 'px-4 md:px-24'
+            !fullWidth ? 'max-w-[45rem] px-4 md:px-0' : 'px-4 md:px-24'
           }`}
         >
           {children}
