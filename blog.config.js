6f170bbb84327904888154fe1935dc10a48b6c4d
ycjcl868 <chaolinjin@gmail.com>
2022-12-31 18:24:38 +0800

    chore: ga

--- blog.config.js
+++ blog.config.js
@@ -27,15 +27,15 @@ const BLOG = {
   notionPageId: process.env.NOTION_PAGE_ID, // DO NOT CHANGE THIS！！！
   notionAccessToken: process.env.NOTION_ACCESS_TOKEN, // Useful if you prefer not to make your database public
   analytics: {
-    providers: ['ga', 'cnzz'], // Currently we support Google Analytics and Ackee, please fill with 'ga' or 'ackee', leave it empty to disable it.
+    providers: ['cnzz'], // Currently we support Google Analytics and Ackee, please fill with 'ga' or 'ackee', leave it empty to disable it.
     ackeeConfig: {
       tracker: '', // e.g 'https://ackee.craigary.net/tracker.js'
       dataAckeeServer: '', // e.g https://ackee.craigary.net , don't end with a slash
       domainId: '' // e.g '0e2257a8-54d4-4847-91a1-0311ea48cc7b'
     },
-    gaConfig: {
-      measurementId: 'G-QNHPPR60EZ' // e.g: G-XXXXXXXXXX
-    },
+    // gaConfig: {
+    //   measurementId: 'G-QNHPPR60EZ' // e.g: G-XXXXXXXXXX
+    // },
     cnzzConfig: {
       id: '1279745642'
     }
