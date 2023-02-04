2b2a89a83b3d94e6a57770b79aa8ce6baec12b7b
ycjcl868 <chaolinjin@gmail.com>
2023-02-04 17:04:12 +0800

    chore: tls

--- pages/api/proxy_github/[...url].ts
+++ pages/api/proxy_github/[...url].ts
@@ -1,12 +1,7 @@
 import axios from 'axios'
-import https from 'https'
 import type { NextApiRequest, NextApiResponse } from 'next'
 import queryString from 'query-string'
 import BLOG from '@/blog.config'
-
-const agent = new https.Agent({
-  rejectUnauthorized: false
-})
 
 export default async function handler(
   req: NextApiRequest,
@@ -18,11 +13,10 @@ export default async function handler(
   const path = Array.isArray(queryUrl) ? queryUrl.join('/') : ''
   const url = `https://api.github.com/${path}?${queryString.stringify(query)}`
 
-  console.log('url', url)
+  console.error('url', url)
 
   const response = await axios(url, {
     method,
-    httpsAgent: agent,
     ...(method === 'POST' ? { data: JSON.stringify(body) } : {}),
     headers: headers as any,
     auth: {
