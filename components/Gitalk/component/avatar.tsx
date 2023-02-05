b9dcd486fdea207f1c17570a037babe550a25565
ycjcl868 <chaolinjin@gmail.com>
2023-02-05 17:03:16 +0800

    fix: gitalk default avatar

--- components/Gitalk/component/avatar.tsx
+++ components/Gitalk/component/avatar.tsx
@@ -1,12 +1,8 @@
 import { useState } from 'react'
 import Image from 'next/image'
+import GithubSVG from '../assets/icon/github.svg'
 
-const Avatar = ({
-  src,
-  className,
-  alt,
-  defaultSrc = '//cdn.jsdelivr.net/npm/gitalk@1/src/assets/icon/github.svg'
-}) => {
+const Avatar = ({ src, className, alt, defaultSrc = GithubSVG }) => {
   const [imgSrc, setImageSrc] = useState(src || defaultSrc)
   return (
     <div className={`gt-avatar ${className}`}>
