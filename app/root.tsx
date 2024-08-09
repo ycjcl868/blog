1982121bafdc7bb7a3c10ee31971d4463d9cb258
ycjcl868 <chaolinjin@gmail.com>
2024-08-09 16:28:14 +0800

    chore: cache

--- app/root.tsx
+++ app/root.tsx
@@ -4,184 +4,184 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-  useNavigation
-} from '@remix-run/react'
-import { ThemeProvider } from 'remix-theme'
-import { useEffect, lazy, Suspense } from 'react'
-import { ClientOnly } from '~/components/ClientOnly'
-import { LinksFunction } from '@remix-run/cloudflare'
-import { IconContext } from 'react-icons'
-import NProgress from 'nprogress'
-import 'prismjs'
-import nProgressStyles from 'nprogress/nprogress.css?url'
-import notionStyles from 'react-notion-x/src/styles.css?url'
-import katexStyles from 'katex/dist/katex.min.css?url'
-import { LocaleProvider } from '~/libs/locale'
-import '~/styles/globals.css'
-import '~/styles/notion.css'
-import '~/styles/gitalk.css'
-import BLOG from '#/blog.config'
-import CJK from '#/cjk'
-import './tailwind.css'
+  useNavigation,
+} from "@remix-run/react";
+import { ThemeProvider } from "remix-theme";
+import { useEffect, lazy, Suspense } from "react";
+import { ClientOnly } from "~/components/ClientOnly";
+import { LinksFunction } from "@remix-run/cloudflare";
+import { IconContext } from "react-icons";
+import NProgress from "nprogress";
+import "prismjs";
+import nProgressStyles from "nprogress/nprogress.css?url";
+import notionStyles from "react-notion-x/src/styles.css?url";
+import katexStyles from "katex/dist/katex.min.css?url";
+import { LocaleProvider } from "~/libs/locale";
+import "~/styles/globals.css";
+import "~/styles/notion.css";
+import "~/styles/gitalk.css";
+import BLOG from "#/blog.config";
+import CJK from "#/cjk";
+import "./tailwind.css";
 
 export const links: LinksFunction = () => [
-  { rel: 'stylesheet', href: nProgressStyles },
-  { rel: 'stylesheet', href: notionStyles },
-  { rel: 'stylesheet', href: katexStyles }
-]
+  { rel: "stylesheet", href: nProgressStyles },
+  { rel: "stylesheet", href: notionStyles },
+  { rel: "stylesheet", href: katexStyles },
+];
 
-const Ackee = lazy(() => import('~/components/Ackee'))
+const Ackee = lazy(() => import("~/components/Ackee"));
 
 export function Layout({ children }: { children: React.ReactNode }) {
-  const url = BLOG.path.length ? `${BLOG.link}/${BLOG.path}` : BLOG.link
+  const url = BLOG.path.length ? `${BLOG.link}/${BLOG.path}` : BLOG.link;
   const meta = {
     title: BLOG.title,
-    type: 'website'
-  }
+    type: "website",
+  };
   const image = `${BLOG.ogImageGenerateURL}/${encodeURIComponent(
     meta.title
-  )}.png?theme=dark&md=1&fontSize=125px&images=https%3A%2F%2Fnobelium.vercel.app%2Flogo-for-dark-bg.svg`
+  )}.png?theme=dark&md=1&fontSize=125px&images=https%3A%2F%2Fnobelium.vercel.app%2Flogo-for-dark-bg.svg`;
 
   return (
-    <html lang='en'>
+    <html lang="en">
       <head>
         <title>{meta.title}</title>
-        <meta name='robots' content='follow, index' />
-        <meta charSet='UTF-8' />
+        <meta name="robots" content="follow, index" />
+        <meta charSet="UTF-8" />
         {BLOG.seo.googleSiteVerification && (
           <meta
-            name='google-site-verification'
+            name="google-site-verification"
             content={BLOG.seo.googleSiteVerification}
           />
         )}
         {BLOG.seo.keywords && (
-          <meta name='keywords' content={BLOG.seo.keywords.join(', ')} />
+          <meta name="keywords" content={BLOG.seo.keywords.join(", ")} />
         )}
-        <meta name='description' content={meta.description} />
-        <meta property='og:locale' content={BLOG.lang} />
-        <meta property='og:title' content={meta.title} />
-        <meta property='og:description' content={meta.description} />
+        <meta name="description" content={meta.description} />
+        <meta property="og:locale" content={BLOG.lang} />
+        <meta property="og:title" content={meta.title} />
+        <meta property="og:description" content={meta.description} />
         <meta
-          property='og:url'
+          property="og:url"
           content={meta.slug ? `${url}/${meta.slug}` : url}
         />
-        <meta property='og:type' content={meta.type} />
-        <meta name='twitter:card' content='summary_large_image' />
-        <meta name='twitter:description' content={meta.description} />
-        <meta name='twitter:title' content={meta.title} />
-        <meta name='twitter:image' content={image} />
-        <meta property='og:image' content={image} />
-        {meta.type === 'article' && (
+        <meta property="og:type" content={meta.type} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:description" content={meta.description} />
+        <meta name="twitter:title" content={meta.title} />
+        <meta name="twitter:image" content={image} />
+        <meta property="og:image" content={image} />
+        {meta.type === "article" && (
           <>
             <meta
-              property='article:published_time'
+              property="article:published_time"
               content={meta.date || meta.createdTime}
             />
-            <meta property='article:author' content={BLOG.author} />
+            <meta property="article:author" content={BLOG.author} />
           </>
         )}
-        <meta charSet='utf-8' />
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
-        {BLOG.font && BLOG.font === 'serif' ? (
+        {BLOG.font && BLOG.font === "serif" ? (
           <>
             <link
-              rel='preload'
-              href='/fonts/SourceSerif.var.woff2'
-              as='font'
-              type='font/woff2'
-              crossOrigin='anonymous'
+              rel="preload"
+              href="/fonts/SourceSerif.var.woff2"
+              as="font"
+              type="font/woff2"
+              crossOrigin="anonymous"
             />
             <link
-              rel='preload'
-              href='/fonts/SourceSerif-Italic.var.woff2'
-              as='font'
-              type='font/woff2'
-              crossOrigin='anonymous'
+              rel="preload"
+              href="/fonts/SourceSerif-Italic.var.woff2"
+              as="font"
+              type="font/woff2"
+              crossOrigin="anonymous"
             />
           </>
         ) : (
           <>
             <link
-              rel='preload'
-              href='/fonts/IBMPlexSansVar-Roman.woff2'
-              as='font'
-              type='font/woff2'
-              crossOrigin='anonymous'
+              rel="preload"
+              href="/fonts/IBMPlexSansVar-Roman.woff2"
+              as="font"
+              type="font/woff2"
+              crossOrigin="anonymous"
             />
             <link
-              rel='preload'
-              href='/fonts/IBMPlexSansVar-Italic.woff2'
-              as='font'
-              type='font/woff2'
-              crossOrigin='anonymous'
+              rel="preload"
+              href="/fonts/IBMPlexSansVar-Italic.woff2"
+              as="font"
+              type="font/woff2"
+              crossOrigin="anonymous"
             />
           </>
         )}
 
-        {['zh', 'ja', 'ko'].includes(
+        {["zh", "ja", "ko"].includes(
           BLOG.lang.slice(0, 2).toLocaleLowerCase()
         ) && (
           <>
             <link
-              rel='preconnect'
-              href='https://fonts.gstatic.com'
-              crossOrigin='anonymous'
+              rel="preconnect"
+              href="https://fonts.gstatic.com"
+              crossOrigin="anonymous"
             />
             <link
-              rel='preload preconnect'
-              as='style'
+              rel="preload preconnect"
+              as="style"
               href={`https://fonts.loli.net/css2?family=Noto+${
-                BLOG.font === 'serif' ? 'Serif' : 'Sans'
+                BLOG.font === "serif" ? "Serif" : "Sans"
               }+${CJK()}:wght@400;500;700&display=swap`}
             />
             <link
-              rel='stylesheet preconnect prefetch'
+              rel="stylesheet preconnect prefetch"
               href={`https://fonts.loli.net/css2?family=Noto+${
-                BLOG.font === 'serif' ? 'Serif' : 'Sans'
+                BLOG.font === "serif" ? "Serif" : "Sans"
               }+${CJK()}:wght@400;500;700&display=swap`}
             />
             <noscript>
               <link
-                rel='stylesheet preconnect prefetch'
+                rel="stylesheet preconnect prefetch"
                 href={`https://fonts.loli.net/css2?family=Noto+${
-                  BLOG.font === 'serif' ? 'Serif' : 'Sans'
+                  BLOG.font === "serif" ? "Serif" : "Sans"
                 }+${CJK()}:wght@400;500;700&display=swap`}
               />
             </noscript>
           </>
         )}
-        <link rel='icon' href='/favicon.png' />
+        <link rel="icon" href="/favicon.png" />
         <link
-          rel='apple-touch-icon'
-          sizes='192x192'
-          href='/apple-touch-icon.png'
+          rel="apple-touch-icon"
+          sizes="192x192"
+          href="/apple-touch-icon.png"
         ></link>
         <link
-          rel='alternate'
-          type='application/rss+xml'
-          title='RSS 2.0'
-          href='/atom.xml'
+          rel="alternate"
+          type="application/rss+xml"
+          title="RSS 2.0"
+          href="/atom.xml"
         ></link>
-        {BLOG.appearance === 'auto' ? (
+        {BLOG.appearance === "auto" ? (
           <>
             <meta
-              name='theme-color'
+              name="theme-color"
               content={BLOG.lightBackground}
-              media='(prefers-color-scheme: light)'
+              media="(prefers-color-scheme: light)"
             />
             <meta
-              name='theme-color'
+              name="theme-color"
               content={BLOG.darkBackground}
-              media='(prefers-color-scheme: dark)'
+              media="(prefers-color-scheme: dark)"
             />
           </>
         ) : (
           <meta
-            name='theme-color'
+            name="theme-color"
             content={
-              BLOG.appearance === 'dark'
+              BLOG.appearance === "dark"
                 ? BLOG.darkBackground
                 : BLOG.lightBackground
             }
@@ -194,38 +194,40 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Scripts />
       </body>
     </html>
-  )
+  );
 }
 
 export default function App() {
-  const navigation = useNavigation()
+  const navigation = useNavigation();
   useEffect(() => {
-    if (navigation.state === 'loading' || navigation.state === 'submitting') {
-      NProgress.start()
+    if (navigation.state === "loading" || navigation.state === "submitting") {
+      NProgress.start();
     } else {
-      NProgress.done()
+      NProgress.done();
     }
-  }, [navigation.state])
+  }, [navigation.state]);
 
   return (
-    <IconContext.Provider value={{ style: { verticalAlign: 'middle' } }}>
+    <IconContext.Provider value={{ style: { verticalAlign: "middle" } }}>
       <LocaleProvider>
         <>
-          {BLOG.isProd && BLOG?.analytics?.providers.includes('ackee') && (
+          {BLOG.isProd && BLOG?.analytics?.providers.includes("ackee") && (
             <ClientOnly>
-              <Suspense fallback=''>
-                <Ackee
-                  ackeeServerUrl={BLOG.analytics.ackeeConfig.dataAckeeServer}
-                  ackeeDomainId={BLOG.analytics.ackeeConfig.domainId}
-                />
-              </Suspense>
+              {() => (
+                <Suspense fallback="">
+                  <Ackee
+                    ackeeServerUrl={BLOG.analytics.ackeeConfig.dataAckeeServer}
+                    ackeeDomainId={BLOG.analytics.ackeeConfig.domainId}
+                  />
+                </Suspense>
+              )}
             </ClientOnly>
           )}
-          <ThemeProvider attribute='class'>
+          <ThemeProvider attribute="class">
             <Outlet />
           </ThemeProvider>
         </>
       </LocaleProvider>
     </IconContext.Provider>
-  )
+  );
 }
--- app/routes/$slug.tsx
+++ app/routes/$slug.tsx
@@ -1,19 +1,19 @@
-import Layout from '~/layouts/layout'
-import { json, type LoaderFunctionArgs } from '@remix-run/cloudflare'
-import { getPostBlocks, getPost } from '~/libs/notion'
+import Layout from "~/layouts/layout";
+import { json, type LoaderFunctionArgs } from "@remix-run/cloudflare";
+import { getPostBlocks, getPost } from "~/libs/notion";
 import {
   getPageTableOfContents,
   uuidToId,
-  getPageImageUrls
-} from 'notion-utils'
-import { PageBlock, Block } from 'notion-types'
-import { mapImageUrl } from '~/libs/utils'
-import { useLoaderData } from '@remix-run/react'
+  getPageImageUrls,
+} from "notion-utils";
+import { PageBlock, Block } from "notion-types";
+import { mapImageUrl } from "~/libs/utils";
+import { useLoaderData } from "@remix-run/react";
 
 const BlogPost = () => {
   const { post, coverImage, blockMap, tableOfContent } =
-    useLoaderData<typeof loader>()
-  if (!post) return null
+    useLoaderData<typeof loader>();
+  if (!post) return null;
   return (
     <Layout
       blockMap={blockMap}
@@ -22,46 +22,53 @@ const BlogPost = () => {
       frontMatter={post}
       fullWidth={post.fullWidth}
     />
-  )
-}
+  );
+};
 
 export const loader = async (params: LoaderFunctionArgs) => {
-  const { slug } = params.params
-  const { NOTION_ACCESS_TOKEN, NOTION_PAGE_ID } = params.context.cloudflare.env
+  const { slug } = params.params;
+  const { NOTION_ACCESS_TOKEN, NOTION_PAGE_ID } = params.context.cloudflare.env;
   const [post] = await getPost({
     slug,
     notionPageId: NOTION_PAGE_ID,
-    notionAccessToken: NOTION_ACCESS_TOKEN
-  })
+    notionAccessToken: NOTION_ACCESS_TOKEN,
+  });
 
   if (!post) {
-    throw new Response('', { status: 404 })
+    throw new Response("", { status: 404 });
   }
 
-  const blockMap = await getPostBlocks(post.id)
+  const blockMap = await getPostBlocks(post.id);
 
-  const [coverImage = ''] =
+  const [coverImage = ""] =
     getPageImageUrls(blockMap, {
       mapImageUrl: (url: string, block: Block) => {
         if (block.format?.page_cover) {
-          return mapImageUrl(url, block)
+          return mapImageUrl(url, block);
         }
-      }
-    }) || []
+      },
+    }) || [];
 
-  const keys = Object.keys(blockMap?.block || {})
-  const block = blockMap?.block?.[keys[0]]?.value as PageBlock
+  const keys = Object.keys(blockMap?.block || {});
+  const block = blockMap?.block?.[keys[0]]?.value as PageBlock;
 
   const tableOfContent =
     getPageTableOfContents(block, blockMap)?.map(
       ({ id, text, indentLevel }) => ({
         id: uuidToId(id),
         text,
-        indentLevel
+        indentLevel,
       })
-    ) || []
+    ) || [];
 
-  return json({ post, blockMap, coverImage, tableOfContent })
-}
+  return json(
+    { post, blockMap, coverImage, tableOfContent },
+    {
+      headers: {
+        "Cache-Control": "public, stale-while-revalidate=60",
+      },
+    }
+  );
+};
 
-export default BlogPost
+export default BlogPost;
