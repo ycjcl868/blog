2a95709aac45a09d01423ea643f7707dabc0579a
ycjcl868 <chaolinjin@gmail.com>
2024-08-09 23:53:58 +0800

    chore: update

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,30 @@
 import {
   vitePlugin as remix,
-  cloudflareDevProxyVitePlugin as remixCloudflareDevProxy
-} from '@remix-run/dev'
-import { defineConfig } from 'vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
+  cloudflareDevProxyVitePlugin as remixCloudflareDevProxy,
+} from "@remix-run/dev";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import legacy from "@vitejs/plugin-legacy";
 
 export default defineConfig({
   plugins: [
+    legacy({
+      targets: ["defaults", "not IE 11"],
+      polyfills: ["es/object/has-own"],
+      modernPolyfills: ["es/object/has-own"],
+    }),
     remixCloudflareDevProxy(),
     remix({
-      serverDependenciesToBundle: [/remix-utils/],
       future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
-        v3_throwAbortReason: true
-      }
+        v3_throwAbortReason: true,
+      },
     }),
-    tsconfigPaths()
-  ]
-})
+    tsconfigPaths(),
+  ],
+  build: {
+    minify: true,
+    cssMinify: true,
+  },
+});
