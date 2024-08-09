b6e69e5d2b331314cfd75d7dd5e3bc26c8cdd54a
ycjcl868 <chaolinjin@gmail.com>
2024-08-09 16:26:10 +0800

    chore: compoent

--- app/components/Comments.tsx
+++ app/components/Comments.tsx
@@ -1,33 +1,28 @@
-import BLOG from '#/blog.config'
-import { useEffect, lazy, Suspense } from 'react'
-import { ClientOnly } from '~/components/ClientOnly'
-import { useTheme } from 'remix-theme'
-import { ReactCusdis } from 'react-cusdis'
-import { useLocation } from '@remix-run/react'
+import BLOG from "#/blog.config";
+import { useEffect, lazy, Suspense } from "react";
+import { useTheme } from "remix-theme";
+import { ReactCusdis } from "react-cusdis";
+import { useLocation } from "@remix-run/react";
 
-const GitalkComponent = lazy(() => {
-  return import('~/components/Gitalk')
-})
-const UtterancesComponent = lazy(() => {
-  return import('~/components/Utterances')
-})
+const GitalkComponent = lazy(() => import("~/components/Gitalk"));
+const UtterancesComponent = lazy(() => import("~/components/Utterances"));
 
 const Comments = ({ frontMatter }) => {
-  const location = useLocation()
-  const { theme } = useTheme()
+  const location = useLocation();
+  const { theme } = useTheme();
 
-  const cusdisTheme = theme === 'dark' ? 'dark' : 'light'
+  const cusdisTheme = theme === "dark" ? "dark" : "light";
 
   useEffect(() => {
-    if (typeof window !== 'undefined' && window.CUSDIS) {
-      window.CUSDIS.setTheme(cusdisTheme)
+    if (typeof window !== "undefined" && window.CUSDIS) {
+      window.CUSDIS.setTheme(cusdisTheme);
     }
-  }, [cusdisTheme])
+  }, [cusdisTheme]);
 
   return (
     <div>
-      {BLOG.comment && BLOG.comment.provider === 'gitalk' && (
-        <Suspense fallback=''>
+      {BLOG.comment && BLOG.comment.provider === "gitalk" && (
+        <Suspense fallback="">
           <GitalkComponent
             options={{
               id: frontMatter.id,
@@ -39,31 +34,31 @@ const Comments = ({ frontMatter }) => {
               admin: BLOG.comment.gitalkConfig.admin,
               distractionFreeMode:
                 BLOG.comment.gitalkConfig.distractionFreeMode,
-              proxy: BLOG.comment.gitalkConfig.proxy
+              proxy: BLOG.comment.gitalkConfig.proxy,
             }}
           />
         </Suspense>
       )}
-      {BLOG.comment && BLOG.comment.provider === 'utterances' && (
+      {BLOG.comment && BLOG.comment.provider === "utterances" && (
         <UtterancesComponent issueTerm={frontMatter.id} />
       )}
-      {BLOG.comment && BLOG.comment.provider === 'cusdis' && (
-        <Suspense fallback=''>
+      {BLOG.comment && BLOG.comment.provider === "cusdis" && (
+        <Suspense fallback="">
           <ReactCusdis
-            lang='zh-cn'
+            lang="zh-cn"
             attrs={{
               host: BLOG.comment.cusdisConfig.host,
               appId: BLOG.comment.cusdisConfig.appId,
               pageId: frontMatter.id,
               pageTitle: frontMatter.title,
               pageUrl: BLOG.link + location.pathname,
-              theme: 'auto'
+              theme: "auto",
             }}
           />
         </Suspense>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default Comments
+export default Comments;
